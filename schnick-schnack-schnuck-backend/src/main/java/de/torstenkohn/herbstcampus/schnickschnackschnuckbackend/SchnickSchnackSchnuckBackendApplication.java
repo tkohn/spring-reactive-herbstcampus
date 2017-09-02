cea0119aa1f7b813266c6b08067c13ba73beafe9
Torsten Kohn <torsten.kohn@comsysto.com>
2017-09-02 14:13:18 +0200

    create spring boot demo backend with start.spring.io
    * add the new module to the root pom

---
+++ schnick-schnack-schnuck-backend/src/main/java/de/torstenkohn/herbstcampus/schnickschnackschnuckbackend/SchnickSchnackSchnuckBackendApplication.java
@@ -0,0 +1,12 @@
+package de.torstenkohn.herbstcampus.schnickschnackschnuckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchnickSchnackSchnuckBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchnickSchnackSchnuckBackendApplication.class, args);
+  }
+}
